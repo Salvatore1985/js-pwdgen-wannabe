445803ca05db6fdd88fc7e0c3b3c176a79a1af2e
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-02-21 13:53:48 +0100

    add js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//*RECUPERO L'ELEMENTO DAL DOM
+
+let elementName = document.getElementById("name");
+let elementLastName = document.getElementById("last-name");
+let elementColor = document.getElementById("my-color");
+
+//* CHIEDO ALL'UTENTE DI INSERIRE I DATI
+const userName = prompt("Qual'è il tuo Nome ?", "Salvatore");
+console.log(userName);
+
+const userLastName = prompt("Qual'è il tuo Cognome ?", "Cascone");
+console.log(userLastName);
+
+const userColor = prompt("Qual'è il tuo colore preferito ?", "Verde");
+console.log(userColor);
+
+
+//* INSERISCO I DATI DEL DOM
+
+elementName.innerHTML = userName;
+elementLastName.innerHTML = userLastName;
+elementColor.innerHTML = userColor;
